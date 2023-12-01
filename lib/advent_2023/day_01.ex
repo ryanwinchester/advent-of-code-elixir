930741156ac394ed59b834bac974ef1c989e98a4
Ryan Winchester <ryan@winchester.dev>
2023-12-01 14:50:19 -0400

    Comment

--- lib/advent_2023/day_01.ex
+++ lib/advent_2023/day_01.ex
@@ -33,6 +33,8 @@ defmodule Advent2023.Day01 do
   defp first_digit(<<d, _rest::binary>>) when d in ?0..?9, do: d - ?0
   defp first_digit(<<_, rest::binary>>), do: first_digit(rest)
 
+  # I don't care about UTF-8 or graphemes and this should perform better than
+  # list, string, or character reversing.
   defp binary_reverse(binary) do
     binary
     |> :binary.decode_unsigned(:little)
@@ -63,8 +65,8 @@ defmodule Advent2023.Day01 do
   end
 
   # Save the first and last digit as we recurse through the string.
-  # Gotcha: the last letter of one number word could be the start of another number word.
-  # I concat them to `rest` when that is the case, and continue the recursion.
+  # Gotcha: the last letter of a number word could be the start of another number word.
+  # I concat those cases to `rest` when that is the case, and continue the recursion.
   defp first_last(string, first \\ nil, last \\ nil)
   defp first_last("one" <> rest, f, _l), do: first_last("e" <> rest, f || 1, 1)
   defp first_last("two" <> rest, f, _l), do: first_last("o" <> rest, f || 2, 2)
